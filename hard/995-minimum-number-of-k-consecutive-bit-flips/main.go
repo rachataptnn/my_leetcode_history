a60befe707568ce3a727779b18688ef48d3261fc
rachataJack <jack.p@q-chang.com>
2024-06-25 09:16:22 +0700

    995/ done for 3 testcases from leetcode, remove comment

--- hard/995-minimum-number-of-k-consecutive-bit-flips/main.go
+++ hard/995-minimum-number-of-k-consecutive-bit-flips/main.go
@@ -19,15 +19,9 @@ func main() {
 func minKBitFlips(nums []int, k int) int {
 	flipCnt := 0
 
-	i := 0
 	for {
 		index := findFirstZero(nums)
 		if index == -1 {
-			fmt.Println("zero not found")
-			if flipCnt == 0 {
-				fmt.Println("flip count = 0")
-				return -1
-			}
 			return flipCnt
 		}
 
@@ -36,17 +30,9 @@ func minKBitFlips(nums []int, k int) int {
 			return -1
 		}
 
-		flippedSlice := flip(subSlice)
-
-		for j := 0; j < len(flippedSlice); j++ {
-			nums[i] = subSlice[j]
-			i++
-		}
-
-		fmt.Println(flippedSlice)
+		flip(subSlice)
+		flipCnt++
 	}
-
-	return 0
 }
 
 func findFirstZero(arr []int) int {
@@ -66,7 +52,7 @@ func getSubSlice(arr []int, k int, index int) ([]int, error) {
 	return arr[index : index+k], nil
 }
 
-func flip(arr []int) []int {
+func flip(arr []int) {
 	for i := 0; i < len(arr); i++ {
 		switch arr[i] {
 		case 0:
@@ -78,51 +64,4 @@ func flip(arr []int) []int {
 			arr[i] = -1
 		}
 	}
-
-	return arr
 }
-
-// k เป็น int ที่จำกำหนดความยาวของ sub array
-// ทีนี้โจทย์คือ
-// ต้องทำให้ array nums เป็น 1 ทั้งก้อน [1, 1, 1, ..., 1]
-// แต่เปลี่ยนได้ทีละ [] sub array ที่ความยาวจำกัดที่ k นะ
-
-// เช่นๆ
-// Example 1
-// =========
-// Input: nums = [0,1,0], k = 1
-// Output: 2
-// Explanation: Flip nums[0], then flip nums[2].
-// ไม่มีไรอันนี้ให้ sub array len = 1 สบายเลย
-// เปลี่ยนทีละตัวให้เป็น 1
-// therefore -> minimum flip = 2
-
-// Example 2
-// =========
-// Input: nums = [1,1,0], k = 2
-// Output: -1
-// Explanation: No matter how we flip subarrays of size 2, we cannot make the array become [1,1,1].
-// อันนี้ก็ไม่มีไร คือแบบ มึงมี array num ยาว 3 อะ
-// เราอยากทำ 1 หมด
-// สมมติเขียนมือนะ
-// มึงฟลิปทีละ 2 element นะ
-// ฟลิปทีนึงได้ 000 งี้
-// ฟลิกอีกที 011, 110 งี้
-// ยังไงก็ 111 ไม่ได้
-// therefore -> minimum flip = -1 กุทำ all 1 ไม่ได้
-
-// Example 3
-// =========
-//                0 1 2 3 4 5 6 7
-// Input: nums = [0,0,0,1,0,1,1,0], k = 3
-// Output: 3
-// Explanation:
-// Flip nums[0],nums[1],nums[2]: nums becomes [1,1,1,1,0,1,1,0]
-
-// Flip nums[4],nums[5],nums[6]:
-// อันนี้ flip nums[4] (0) -> 1
-// flip nums[5] (1) -> 0
-// flip nums[6] (1) -> 0
-// nums ก็จะเป็น [1,1,1,1,1,0,0,0]
-
-// Flip nums[5],nums[6],nums[7]: nums becomes [1,1,1,1,1,1,1,1]
--- hard/995-minimum-number-of-k-consecutive-bit-flips/main_test.go
+++ hard/995-minimum-number-of-k-consecutive-bit-flips/main_test.go
@@ -5,27 +5,51 @@ import (
 )
 
 func TestMinKBitFlips(t *testing.T) {
-	tests := []struct {
-		A        []int
-		K        int
+	type testCasesStruct struct {
+		caseName string
+		nums     []int
+		k        int
 		expected int
-	}{
-		// Test cases
-		{[]int{0, 1, 0}, 1, 2},
-		{[]int{1, 1, 0}, 2, -1},
-		{[]int{0, 0, 0, 1, 0, 1, 1, 0}, 3, 3},
-		{[]int{1, 0, 1, 0, 1, 0, 1}, 3, 2},
-		{[]int{0, 0, 0, 0, 0}, 2, 3},
-		{[]int{0, 0, 0, 0, 0}, 3, 2},
-		{[]int{1, 1, 1, 1, 1}, 1, 0},
-		{[]int{1, 0, 0, 0, 1, 0}, 3, -1},
 	}
 
-	for _, tt := range tests {
+	testCases := []testCasesStruct{
+		{
+			caseName: "default case from Leetcode 1",
+			nums:     []int{0, 1, 0},
+			k:        1,
+			expected: 2,
+		},
+		{
+			caseName: "default case from Leetcode 2",
+			nums:     []int{1, 1, 0},
+			k:        2,
+			expected: -1,
+		},
+		{
+			caseName: "default case from Leetcode 3",
+			nums:     []int{0, 0, 0, 1, 0, 1, 1, 0},
+			k:        3,
+			expected: 3,
+		},
+	}
+
+	// {
+	// 	// Test cases
+	// 	{[]int{0, 1, 0}, 1, 2},
+	// 	{[]int{1, 1, 0}, 2, -1},
+	// 	{[]int{0, 0, 0, 1, 0, 1, 1, 0}, 3, 3},
+	// 	{[]int{1, 0, 1, 0, 1, 0, 1}, 3, 2},
+	// 	{[]int{0, 0, 0, 0, 0}, 2, 3},
+	// 	{[]int{0, 0, 0, 0, 0}, 3, 2},
+	// 	{[]int{1, 1, 1, 1, 1}, 1, 0},
+	// 	{[]int{1, 0, 0, 0, 1, 0}, 3, -1},
+	// }
+
+	for _, tt := range testCases {
 		t.Run("", func(t *testing.T) {
-			result := minKBitFlips(tt.A, tt.K)
+			result := minKBitFlips(tt.nums, tt.k)
 			if result != tt.expected {
-				t.Errorf("minKBitFlips(%v, %d) = %d; want %d", tt.A, tt.K, result, tt.expected)
+				t.Errorf("minKBitFlips(%v, %d) = %d; want %d", tt.nums, tt.k, result, tt.expected)
 			}
 		})
 	}
