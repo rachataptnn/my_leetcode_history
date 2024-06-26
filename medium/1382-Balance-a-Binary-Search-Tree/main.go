e5011ba98d194aefd2fb83402a2bff93c82476ca
rachataptnn <rachata.ptnn@gmail.com>
2024-06-26 13:53:01 +0700

    1382/done

--- medium/1382-Balance-a-Binary-Search-Tree/main.go
+++ medium/1382-Balance-a-Binary-Search-Tree/main.go
@@ -1,22 +1,76 @@
-// https://leetcode.com/problems/balance-a-binary-search-tree/description/?envType=daily-question&envId=2024-06-26
-
 package main
 
+import (
+	"fmt"
+)
+
+// TreeNode represents a node in the binary search tree
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func main() {
-	tn := TreeNode{
-		Val:   1,
-		Left:  nil,
-		Right: nil,
-	}
-	balanceBST(&tn)
+func balanceBST(root *TreeNode) *TreeNode {
+	var values []int
+	inorder(root, &values)
+	return buildBalancedBST(values, 0, len(values)-1)
 }
 
-func balanceBST(root *TreeNode) *TreeNode {
-	return nil // //
+func inorder(node *TreeNode, values *[]int) {
+	if node == nil {
+		return
+	}
+	inorder(node.Left, values)
+	*values = append(*values, node.Val)
+	inorder(node.Right, values)
+}
+
+func buildBalancedBST(values []int, left, right int) *TreeNode {
+	if left > right {
+		return nil
+	}
+
+	mid := (left + right) / 2
+	node := &TreeNode{Val: values[mid]}
+
+	node.Left = buildBalancedBST(values, left, mid-1)
+	node.Right = buildBalancedBST(values, mid+1, right)
+
+	return node
+}
+
+// Helper function to print the tree in-order
+func printInorder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	printInorder(node.Left)
+	fmt.Print(node.Val, " ")
+	printInorder(node.Right)
+}
+
+// Helper function to create an unbalanced BST for testing
+func createUnbalancedBST() *TreeNode {
+	root := &TreeNode{Val: 1}
+	root.Right = &TreeNode{Val: 2}
+	root.Right.Right = &TreeNode{Val: 3}
+	root.Right.Right.Right = &TreeNode{Val: 4}
+	return root
+}
+
+func main() {
+	// Create an unbalanced BST
+	unbalancedRoot := createUnbalancedBST()
+
+	fmt.Println("Unbalanced BST (in-order):")
+	printInorder(unbalancedRoot)
+	fmt.Println()
+
+	// Balance the BST
+	balancedRoot := balanceBST(unbalancedRoot)
+
+	fmt.Println("Balanced BST (in-order):")
+	printInorder(balancedRoot)
+	fmt.Println()
 }
