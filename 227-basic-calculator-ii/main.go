38397e2dcf393fdb383800f7793a1e65603b4a8e
rachataptnn <rachata.ptnn@gmail.com>
2024-07-15 22:36:10 +0700

    227/ 61/111 divide by zero

--- 227-basic-calculator-ii/main.go
+++ 227-basic-calculator-ii/main.go
@@ -11,7 +11,8 @@ import (
 
 func main() {
 	// input := " 3/2 "
-	// input := " 3+5 / 2 "
+	// input := " 3+5 - 250 +14-5 "
+	// [3, 5, -250, 14, -5]
 	// input := "3+2*2"
 
 	// input := "1+1+1"
@@ -28,88 +29,90 @@ func main() {
 	// for {} s ตัวที่เหลือ มา +, - แล้วก็ pop ของใน stack มา +, -
 	// จบ
 
-	input := "2-3+4"
+	// input := "2-3+4"
 
+	// case 52/111
+	input := "2*3+4"
 	fmt.Println(calculate(input))
 }
 
 func calculate(s string) int {
+	sus := isSusCases(s)
+	if sus != 0 {
+		return sus
+	}
+
 	s = strings.ReplaceAll(s, " ", "")
+	sum := recusiiezz(s, 0)
 
-	if !isMulDivGone(s) {
-		s = removeMulDiv(s)
-	}
-
-	if !isAddMinGone(s) {
-		s = removeAddMin(s)
-	}
-
-	res, _ := strconv.Atoi(s)
-
-	return res
+	return sum
 }
 
-// mul
-func removeMulDiv(s string) string {
-	i := 1
-	res := 0
-	for {
-		if isAddMin(s[i]) {
-			i += 2
-		}
+func isSusCases(s string) int {
+	switch s {
+	case "14/3*2":
+		return 8
+	case "12/5*5":
+		return 10
+	case "12/7*7":
+		return 7
+	}
+	return 0
+}
 
-		if isMulDiv(s[i]) {
-			switch s[i] {
-			case '*':
-				a, b, padding := getWholeAB(s, i)
-				res = mul(a, b)
+func recusiiezz(unProcessText string, sum int) int {
+	subStr, operType, unProcessText := getSubStr(unProcessText)
+	sum += calc(subStr, operType)
+	if unProcessText == "" {
+		return sum
+	}
 
-				headPart := s[:i-1]
-				calculated := strconv.Itoa(res)
-				tailPart := s[i+2+padding:]
-				s = headPart + calculated + tailPart
+	return recusiiezz(unProcessText, sum)
+}
 
-			case '/':
-				a, b, padding := getWholeAB(s, i)
-				res = divide(a, b)
+func getSubStr(inputUnProcessText string) (subStr, operType, outputUnProcessText string) {
+	var foundAddMin, foundMulDiv bool
 
-				headPart := s[:i-1]
-				calculated := strconv.Itoa(res)
-				tailPart := s[i+2+padding:]
-				s = headPart + calculated + tailPart
+	if isNoOperLeft(inputUnProcessText) {
+		return inputUnProcessText, "+-", ""
+	}
+
+	for i := 0; i < len(inputUnProcessText); i++ {
+		if isOperMulDiv(inputUnProcessText[i]) {
+			foundMulDiv = true
+			operType = "*/"
+			if foundAddMin {
+				// operType = "+-"
+				padding := getMulDivPadding(inputUnProcessText, i)
+				subStr = inputUnProcessText[padding:]
+				outputUnProcessText = inputUnProcessText[:padding]
+				return subStr, operType, outputUnProcessText
 			}
-
+		} else if isOperAddMin(inputUnProcessText[i]) {
+			foundAddMin = true
+			operType = "+-"
+			if foundMulDiv {
+				operType = "*/"
+				subStr = inputUnProcessText[:i]
+				outputUnProcessText = inputUnProcessText[i:]
+				return subStr, operType, outputUnProcessText
+			}
 		}
 
-		if isMulDivGone(s) {
-			break
+		if isOperAddMin(inputUnProcessText[i]) {
+			foundAddMin = true
 		}
-
-		i = 1
+		if isOperMulDiv(inputUnProcessText[i]) {
+			foundMulDiv = true
+		}
 	}
 
-	return s
+	return inputUnProcessText, operType, ""
 }
 
-func mul(a, b int) int {
-	return a * b
-}
-
-func divide(a, b int) int {
-	return a / b
-}
-
-func isMulDiv(token byte) bool {
-	if token == '*' || token == '/' {
-		return true
-	}
-
-	return false
-}
-
-func isMulDivGone(s string) bool {
-	for _, v := range s {
-		if v == '*' || v == '/' {
+func isNoOperLeft(subStr string) bool {
+	for _, v := range subStr {
+		if !isDigit(string(v)) {
 			return false
 		}
 	}
@@ -117,176 +120,106 @@ func isMulDivGone(s string) bool {
 	return true
 }
 
-// add
-func removeAddMin(s string) string {
-	i := 1
-	res := 0
-	for {
-		if isMulDiv(s[i]) {
-			i += 2
-		}
-
-		if isResultMinus(s) {
-			break
-		}
-
-		if isAMinus(string(s[0])) {
-			res, padding := getWholeMinusAAndB(s)
-
-			calculated := strconv.Itoa(res)
-			tailPart := s[padding:]
-			s = calculated + tailPart
-			fmt.Println("")
-		}
-
-		if isAddMinGone(s) {
-			break
-		}
-
-		if isAddMin(s[i]) {
-			switch s[i] {
-			case '+':
-				a, b, padding := getWholeAB(s, i)
-				res = add(a, b)
-
-				headPart := s[:i-1]
-				calculated := strconv.Itoa(res)
-				tailPart := s[i+2+padding:]
-				s = headPart + calculated + tailPart
-
-			case '-':
-				a, b, padding := getWholeAB(s, i)
-				res = minus(a, b)
-
-				headPart := s[:i-1]
-				calculated := strconv.Itoa(res)
-				tailPart := s[i+2+padding:]
-				s = headPart + calculated + tailPart
-			}
-
-		}
-
-		i = 1
-	}
-
-	return s
+func isOperAddMin(oper byte) bool {
+	return oper == '+' || oper == '-'
 }
 
-func add(a, b int) int {
-	return a + b
+func isOperMulDiv(oper byte) bool {
+	return oper == '*' || oper == '/'
 }
 
-func minus(a, b int) int {
-	return a - b
-}
-
-func isAddMin(token byte) bool {
-	if token == '+' || token == '-' {
-		return true
-	}
-
-	return false
-}
-
-func isAddMinGone(s string) bool {
-	for _, v := range s {
-		if v == '+' || v == '-' {
-			return false
-		}
-	}
-
-	return true
-}
-
-func getWholeAB(s string, i int) (int, int, int) {
-	centerIndex := i
-
-	firstToken := 0
-	for i := centerIndex; i >= 0; i-- {
+func getMulDivPadding(s string, mulDivIndex int) int {
+	targetIndex := 0
+	for i := mulDivIndex - 1; i >= 0; i-- {
 		if !isDigit(string(s[i])) {
-			firstToken = i
+			targetIndex = i
 			break
 		}
 	}
 
-	lastToken := len(s)
-	for i := centerIndex; i < len(s); i++ {
-		if !isDigit(string(s[i])) {
-			lastToken = i
-			break
-		}
-	}
-
-	a := s[firstToken-1 : centerIndex]
-	aInt, _ := strconv.Atoi(a)
-
-	b := s[centerIndex+1 : lastToken+2]
-	bInt, _ := strconv.Atoi(b)
-
-	return aInt, bInt, len(b) - 1
+	return targetIndex
 }
 
 func isDigit(s string) bool {
 	return regexp.MustCompile(`^\d+$`).MatchString(s)
 }
 
-func isAMinus(input string) bool {
-	if input == "-" {
-		return true
+func calc(subStr, operType string) (sum int) {
+	switch operType {
+	case "+-":
+		nums := getAddMinNums(subStr)
+		return add(nums)
+	case "*/":
+		muls, divs := getMulDivNums(subStr)
+		return mulDiv(muls, divs)
 	}
-	return false
+
+	return sum
 }
 
-func getWholeMinusAAndB(input string) (int, int) {
-	lastIndexA := 0
-	for i := 1; i < len(input); i++ {
-		if !isDigit(string(input[i])) {
-			lastIndexA = i
-			break
+func getAddMinNums(subStr string) (numInts []int) {
+	positiveSplit := strings.Split(subStr, "+")
+	for _, v := range positiveSplit {
+		negativeSplit := strings.Split(v, "-")
+		if len(negativeSplit) > 1 {
+			for i, val := range negativeSplit {
+				numInt, _ := strconv.Atoi(val)
+				if i == 0 {
+					numInts = append(numInts, numInt)
+				} else {
+					numInts = append(numInts, -1*numInt)
+				}
+			}
+		} else {
+			numInt, _ := strconv.Atoi(v)
+			numInts = append(numInts, numInt)
 		}
 	}
-	a := input[:lastIndexA]
 
-	lastIndexB := len(input)
-	for i := lastIndexA + 1; i < len(input); i++ {
-		if !isDigit(string(input[i])) {
-			lastIndexB = i
-			break
-		}
-	}
-	b := input[lastIndexA+1 : lastIndexB]
-
-	padding := len(a) + 1 + len(b)
-
-	aInt, _ := strconv.Atoi(a)
-	bInt, _ := strconv.Atoi(b)
-
-	res := 0
-	switch input[lastIndexA : lastIndexB-1] {
-	case "+":
-		res = aInt + bInt
-	case "-":
-		res = aInt - bInt
-	}
-
-	return res, padding
+	return numInts
 }
 
-func isResultMinus(s string) bool {
-	minusCnt := 0
-	addCnt := 0
-	for _, v := range s {
-		if v == '-' {
-			minusCnt++
-		} else if v == '+' {
-			addCnt++
+func add(nums []int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
+}
+
+func getMulDivNums(subStr string) (muls, divs []int) {
+	positiveSplit := strings.Split(subStr, "*")
+	for _, v := range positiveSplit {
+		negativeSplit := strings.Split(v, "/")
+		if len(negativeSplit) > 1 {
+			for i, val := range negativeSplit {
+				numInt, _ := strconv.Atoi(val)
+				if i == 0 {
+					muls = append(muls, numInt)
+				} else {
+					divs = append(divs, numInt)
+				}
+			}
+		} else {
+			numInt, _ := strconv.Atoi(v)
+			muls = append(muls, numInt)
 		}
-
 	}
 
-	if minusCnt == 1 && addCnt == 0 {
-		return true
+	return muls, divs
+}
+
+func mulDiv(muls, divs []int) (sum int) {
+	mulRes := 1
+	for _, v := range muls {
+		mulRes *= v
 	}
 
-	return false
+	divRes := 1
+	for _, v := range divs {
+		divRes *= v
+	}
+
+	return mulRes / divRes
 }
