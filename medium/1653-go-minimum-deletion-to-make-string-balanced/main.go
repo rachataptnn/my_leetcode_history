d483b72879ef61b0e7098d9df287daec920a329d
rachataptnn <rachata.ptnn@gmail.com>
2024-07-31 12:37:10 +0700

    1653/undone rip me

---
+++ medium/1653-go-minimum-deletion-to-make-string-balanced/main.go
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/description/?envType=daily-question&envId=2024-07-30
+
+// rip me ;-;
+// after long night i think i knew why requirement gathering is so muuuuuch IMPORTANT
+// waste of my 3-4 hours of solving another problem T T
+
+// the prob desc:
+// "pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'."  <-- da h lol
+
+// someone in discuss just say:
+// "it's basically saying that no 'b' can come before any 'a'"
+// "The use of "pair" in the description is overly complicated"
+
+// the wording 'balance' make me confused so much ._.
+// anyway i would do this prob later. soon.
