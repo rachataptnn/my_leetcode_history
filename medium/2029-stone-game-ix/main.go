a3474253d1e838f789b88751f45a9a9923854d54
rachataptnn <rachata.ptnn@gmail.com>
2024-08-10 22:57:54 +0700

    2029/pass 66/106

--- medium/2029-stone-game-ix/main.go
+++ medium/2029-stone-game-ix/main.go
@@ -2,39 +2,68 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	stones := []int{2, 1}
+	stones := []int{5, 1, 2, 4, 3}
+	// stones := []int{2, 1}
+	// stones := []int{2}
+
+	// 62/106
+	// stones := []int{2, 3}
 	fmt.Println(stoneGameIX(stones))
 }
 
 func stoneGameIX(stones []int) bool {
-	round := pickNum(stones, 0, 1)
-	return round%2 == 0
+	s := states{
+		stones: stones,
+		round:  1,
+		sum:    0,
+	}
+	isAliceWin := s.pickNum()
+	return isAliceWin
 }
 
-func pickNum(stones []int, sum, round int) int {
+type states struct {
+	stones     []int
+	sum        int
+	round      int
+	isAliceWin bool
+}
+
+func (s states) pickNum() bool {
 	selectedNum := 0
 	selectedIdx := 0
-	for i, v := range stones {
-		if sum+v%3 != 0 {
+	for i, v := range s.stones {
+		isStone3Dividable := (s.sum+v)%3 == 0 // player play optimal
+		if !isStone3Dividable {
 			selectedIdx = i
 			selectedNum = v
 
-			sum += v
-			round++
+			s.sum += v
+			s.round++
+
+			s.stones = removeElementByIndex(s.stones, selectedIdx) // the player may remove any stone from stones
 
 			break
 		}
 	}
 
-	if selectedNum == 0 && selectedIdx == 0 { // end case
-		return round
+	// end case: out of stone, left stone make sum divisible by 3
+	if selectedNum == 0 && selectedIdx == 0 {
+		noRemainingStones := len(s.stones) == 0
+		if noRemainingStones {
+			s.isAliceWin = false // Bob will win automatically if: there are no remaining stones
+		} else {
+			isBobTurn := s.round%2 == 0
+			s.isAliceWin = isBobTurn // The player who removes a stone loses if the sum of the values of all removed stones is divisible by 3
+		}
+		return s.isAliceWin
 	}
 
-	stones = removeElementByIndex(stones, selectedIdx)
-	return pickNum(stones, sum, round)
+	return s.pickNum()
 }
 
 func removeElementByIndex(arr []int, index int) []int {
