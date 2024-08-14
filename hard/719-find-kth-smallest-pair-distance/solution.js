f7f063f7fad12d2cf7a8d9ad7654499afa5204ea
rachataptnn <rachata.ptnn@gmail.com>
2024-08-14 15:42:52 +0700

    719/update space btw funcs of js

--- hard/719-find-kth-smallest-pair-distance/solution.js
+++ hard/719-find-kth-smallest-pair-distance/solution.js
@@ -1,6 +1,7 @@
 var nums = [1, 6, 1];
 var k = 3;
 console.log(smallestDistancePair(nums, k));
+
 function smallestDistancePair(nums, k) {
     nums.sort(function (a, b) { return a - b; });
     // Binary Search:
@@ -24,6 +25,7 @@ function smallestDistancePair(nums, k) {
     }
     return minDistance;
 }
+
 function countPairsWithDistanceLessThanOrEqual(nums, targetDistance) {
     var count = 0;
     var right = 0;
