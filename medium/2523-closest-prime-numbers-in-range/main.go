f160d60fe4476e4a1498a55e954a684cdb32ee50
rachataptnn <rachata.ptnn@gmail.com>
2025-01-27 08:41:29 +0700

    2523/Done its fun and algo name is so cool

--- medium/2523-closest-prime-numbers-in-range/main.go
+++ medium/2523-closest-prime-numbers-in-range/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// fmt.Println(closestPrimes(10, 19))
@@ -20,45 +23,100 @@ func closestPrimes(left int, right int) []int {
 		return []int{-1, -1}
 	}
 
-	min := 99999999
+	min := math.MaxInt64
 	firstIndex := -1
 	secondIndex := -1
 
 	firstRes := -1
 	secondRes := -1
 
-	for i := left; i <= right; i++ {
-		if isPrime(i) {
-			if firstIndex == -1 && secondIndex == -1 {
+	if right < 30000 {
+		for i := left; i <= right; i++ {
+			if isPrime(i) {
+				if firstIndex == -1 && secondIndex == -1 {
+					firstIndex = i
+				} else {
+					secondIndex = i
+
+					tmpMin := secondIndex - firstIndex
+					if tmpMin < min {
+						min = tmpMin
+						firstRes, secondRes = firstIndex, secondIndex
+						if tmpMin == 1 {
+							return []int{firstRes, secondRes}
+						}
+					}
+
+					firstIndex = i
+					secondIndex = -1
+				}
+			}
+		}
+
+		return []int{firstRes, secondRes}
+	} else {
+		isPrime := sieveOfEratosthenes(right)
+
+		for i := left; i <= right; i++ {
+			if isPrime[i] && firstIndex == -1 {
 				firstIndex = i
-			} else {
+			} else if isPrime[i] {
 				secondIndex = i
 
 				tmpMin := secondIndex - firstIndex
 				if tmpMin < min {
 					min = tmpMin
 					firstRes, secondRes = firstIndex, secondIndex
+					if tmpMin == 1 {
+						return []int{firstRes, secondRes}
+					}
 				}
 
 				firstIndex = i
 				secondIndex = -1
 			}
 		}
-	}
 
-	return []int{firstRes, secondRes}
+		if firstRes == -1 || secondRes == -1 {
+			return []int{-1, -1}
+		}
+
+		return []int{firstRes, secondRes}
+	}
 }
 
 func isPrime(n int) bool {
 	if n <= 1 {
 		return false
 	}
+	if n <= 3 {
+		return true
+	}
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
 
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
 			return false
 		}
 	}
-
 	return true
+}
+
+func sieveOfEratosthenes(limit int) []bool {
+	isPrime := make([]bool, limit+1)
+	for i := range isPrime {
+		isPrime[i] = true
+	}
+	isPrime[0], isPrime[1] = false, false
+
+	for i := 2; i*i <= limit; i++ {
+		if isPrime[i] {
+			for j := i * i; j <= limit; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+	return isPrime
 }
