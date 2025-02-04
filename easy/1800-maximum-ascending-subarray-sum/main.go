1c613fc9d60f888aa3090edcc8de9dc283700ffc
rachataptnn <rachata.ptnn@gmail.com>
2025-02-04 11:26:13 +0700

    1800/done - just refactored

--- easy/1800-maximum-ascending-subarray-sum/main.go
+++ easy/1800-maximum-ascending-subarray-sum/main.go
@@ -4,39 +4,24 @@ import "fmt"
 
 func main() {
 	nums := []int{10, 20, 30, 5, 10, 50}
+
+	// nums := []int{10, 20, 30, 40, 50}
+
 	fmt.Println(maxAscendingSum(nums))
 }
 
 func maxAscendingSum(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
-	}
+	res, sum := nums[0], nums[0]
 
-	max := 0
-	tmpSum := 0
-	for i := 0; i < len(nums); i++ {
-		if i < len(nums)-1 {
-			if nums[i] < nums[i+1] {
-				tmpSum += nums[i]
-				if max < tmpSum {
-					max = tmpSum
-				}
-			} else {
-				tmpSum += nums[i]
-				if max < tmpSum {
-					max = tmpSum
-				}
-				tmpSum = 0
-			}
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			sum += nums[i]
 		} else {
-			if nums[i] > nums[i-1] {
-				tmpSum += nums[i]
-				if max < tmpSum {
-					max = tmpSum
-				}
-			}
+			sum = nums[i]
 		}
+
+		res = max(res, sum)
 	}
 
-	return max
+	return res
 }
