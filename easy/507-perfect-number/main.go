40dd2d6c5a80c4be11ce1d418f27a6b87f0acc0a
rachataptnn <rachata.ptnn@gmail.com>
2025-02-13 15:35:28 +0700

    2732/pass 97/98: surprised that easy lvl show me time limit exceeded

---
+++ easy/507-perfect-number/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// pass 97/98
+	num := 2732
+	fmt.Println(checkPerfectNumber(num))
+}
+
+func checkPerfectNumber(num int) bool {
+	sum := 0
+	for i := 1; i < num; i++ {
+		if num%i == 0 {
+			sum += i
+		}
+	}
+
+	return sum == num
+}
