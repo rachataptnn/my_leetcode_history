adaa7c71a7f66935e9f33c9381ddff8330aecc20
Rachata Punyathananan <rachata.p@7solution.co.th>
2025-06-01 20:40:05 +0700

    925/pass 65/95: map is the way i guess

--- easy/925-long-pressed-name/main.go
+++ easy/925-long-pressed-name/main.go
@@ -3,28 +3,70 @@ package main
 import "fmt"
 
 func main() {
-	name := "alex"
-	typed := "aaleex"
+	// 1 ex true
+	// name := "alex"
+	// typed := "aaleex"
+
+	// 2 ex false
+	name := "saeed"
+	typed := "ssaaedd"
+
+	// 76/95 ex false
+	// name := "rick"
+	// typed := "kric"
 	fmt.Println(isLongPressedName(name, typed))
 }
 
 func isLongPressedName(name string, typed string) bool {
-	mapName := make(map[string]int)
-	mapTyped := make(map[string]int)
+	n := len(typed)
 
-	for _, v := range name {
-		mapName[string(v)]++
-	}
-	for _, v := range typed {
-		mapTyped[string(v)]++
-	}
+	index := 0
+	for _, nameCh := range name {
+		foundNameMatchTyped := false
+		for index < n {
+			typedCh := rune(typed[index])
 
-	for k, vn := range mapName {
-		vt, ok := mapTyped[k]
-		if !ok || vt < vn {
-			return false
+			if !foundNameMatchTyped {
+				if nameCh == typedCh {
+					foundNameMatchTyped = true
+
+					index++
+					continue
+				} else {
+					return false
+				}
+			} else if nameCh == typedCh {
+				index++
+				continue
+			} else {
+				break
+				// foundNameMatchTyped = false
+				// index++
+				// continue
+			}
 		}
 	}
 
 	return true
 }
+
+// mapName := make(map[string]int)
+// mapTyped := make(map[string]int)
+
+// // isAmountValid
+// for _, v := range name {
+// 	mapName[string(v)]++
+// }
+// for _, v := range typed {
+// 	mapTyped[string(v)]++
+// }
+// for k, vn := range mapName {
+// 	vt, ok := mapTyped[k]
+// 	if !ok || vt < vn {
+// 		return false
+// 	}
+// }
+
+// // isSequenceValid
+
+// return true
