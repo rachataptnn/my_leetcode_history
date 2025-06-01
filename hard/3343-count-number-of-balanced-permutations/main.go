27a00ca2d82f6559690ad2f9a980bfc7ac61eacf
Rachata Punyathananan <rachata.p@7solution.co.th>
2025-06-01 17:14:05 +0700

    3343/pass 2/3

---
+++ hard/3343-count-number-of-balanced-permutations/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	num := "123"
+
+	fmt.Println(countBalancedPermutations(num))
+}
+
+func countBalancedPermutations(num string) int {
+	in := []rune(num)
+	perms := permute(in, 0, len(in)-1)
+	cnt := 0
+
+	for _, v := range perms {
+		if isBalanced(v) {
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+func permute(in []rune, l, r int) []string {
+	var out []string
+	if l == r {
+		out = append(out, string(in))
+	} else {
+		for i := l; i <= r; i++ {
+			in[l], in[i] = in[i], in[l]
+			out = append(out, permute(in, l+1, r)...)
+			in[l], in[i] = in[i], in[l] // backtrack
+		}
+	}
+	return out
+}
+
+func isBalanced(s string) bool {
+	sumOdd := 0
+	sumEven := 0
+
+	for i, v := range s {
+		num, _ := strconv.Atoi(string(v))
+		if i%2 == 0 {
+			sumEven += num
+		} else {
+			sumOdd += num
+		}
+	}
+
+	return sumEven == sumOdd
+}
