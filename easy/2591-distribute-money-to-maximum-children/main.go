9491b6f3537ed467b12b57c34bd6fcd6f9d41820
Jack-Rachata Punyathananan <rachata.p@7solutions.co.th>
2026-07-20 08:46:01 +0700

    2591(612/3802) try to no brain but, enough

--- easy/2591-distribute-money-to-maximum-children/main.go
+++ easy/2591-distribute-money-to-maximum-children/main.go
@@ -12,13 +12,27 @@ func main() {
 
 func distMoney(money int, children int) int {
 	if money < 8 {
+		if money >= children {
+			return 0
+		}
 		return -1
+	}
+
+	if money < children {
+		return -1
+	}
+
+	if money/8 == children {
+		return children
 	}
 
 	childGot8 := 0
 	for v := range children {
 		childNo := v + 1
-		if money < 8 {
+		if money <= 8 {
+			if money == 4 {
+				childGot8--
+			}
 			break
 		}
 		money -= childNo * 8
@@ -31,9 +45,6 @@ func distMoney(money int, children int) int {
 	noMoneyChild := children - childGot8
 	if money >= noMoneyChild {
 		return childGot8
-	} else {
-		return childGot8 - 1
 	}
-
-	return 0
+	return childGot8 - 1
 }
